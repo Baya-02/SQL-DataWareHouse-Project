b51be1dbea6062c1f72972ee77117502f6d690e9
Bayaman <bayamanorozobekov02@gmail.com>
2025-06-02 17:34:46 +0300

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,16 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'.
+    Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+*/
+-- Creating Bronze Layer
+create schema bronze;
+-- Creating Silver Layer
+create schema silver;
+-- Creating Gold Layer
+create schema gold;
